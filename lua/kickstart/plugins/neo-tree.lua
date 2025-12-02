8567f0ff2798c10ff4c1f00438f61f70ea749491
eduardosilva <eduardo.lour.silva@gmail.com>
2025-12-02 19:06:26 -0300

    feat(nvim): enable and configure neo-tree as primary explorer
    Neo-tree was enabled and fully configured to standardize it as the
    primary file explorer in the Neovim setup. Custom keymaps, loading
    behavior, and event handlers were added to streamline navigation and
    file operations, improving workflow consistency and productivity.

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -9,16 +9,52 @@ return {
     'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
     'MunifTanjim/nui.nvim',
   },
-  lazy = false,
+  cmd = 'Neotree',
   keys = {
     { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
   },
   opts = {
     filesystem = {
       window = {
+        -- position = 'float', -- Set window position to 'float'
         mappings = {
-          ['\\'] = 'close_window',
+          -- General file operations
+          ['r'] = 'refresh', -- Refresh NeoTree
+          ['R'] = 'rename', -- Rename file or folder
+          ['d'] = 'delete', -- Delete file or folder
+          ['c'] = 'copy', -- Copy file or folder
+          ['x'] = 'cut_to_clipboard', -- Cut file or folder
+          ['p'] = 'paste_from_clipboard', -- Paste file or folder
+          ['y'] = 'copy_to_clipboard', -- Yank file path
+
+          -- File/folder navigation
+          ['l'] = 'open', -- Expand folder or open file
+          ['h'] = 'close_node', -- Collapse folder
+          ['<CR>'] = 'open', -- Open file or directory
+          ['<BS>'] = 'navigate_up', -- Go up one directory
+          ['P'] = {
+            'toggle_preview',
+            config = {
+              use_float = false,
+            },
+          },
+
+          -- Source navigation
+          ['H'] = 'prev_source', -- Switch to the previous source (buffers, git, etc.)
+          ['L'] = 'next_source', -- Switch to the next source
+
+          -- Hidden files & window management
+          ['.'] = 'toggle_hidden', -- Toggle hidden files
+          ['\\'] = 'close_window', -- Close NeoTree
         },
+      },
+    },
+    event_handlers = {
+      {
+        event = 'file_opened',
+        handler = function()
+          require('neo-tree').close_all() -- Close NeoTree when a file is opened
+        end,
       },
     },
   },
